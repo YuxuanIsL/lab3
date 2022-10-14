7e3056f6580a2e53a767843a5851d701d9ccbfdc
YuxuanIsL <yul208@ucsd.edu>
2022-10-14 11:12:59 -0700

    Create ListTests.java

---
+++ ListTests.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ArrayTests{
+
+    @Test
+    public void testFilter(){
+        List<String> toTest = new ArrayList();
+        StringChecker sc = new StringChecker();
+        
+    }
+
+
+
+}
